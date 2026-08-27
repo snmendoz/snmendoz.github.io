3c4fca139c46c6ece1e125897288548afa46ceff
Sebastián Mendoza Farías <snmendoz@uc.cl>
2026-08-27 17:46:16 -0400

    up

--- README.md
+++
@@ -1,4 +0,0 @@
-# Personal website
-
-Hosted on [snmendoz.github.io](https://snmendoz.github.io)
-
--- src/run-jekyll-local-as-github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# if problems with the version of ruby
-chruby 2.7.2
-# then 
-bundle config set --local path 'vendor/bundle'
-bundle install
-# see https://open-research.gemmadanks.com/tutorials/how-to-use-jekyll-scholar-with-github-pages/
-export LANG=en_US.UTF-8
-bundle exec jekyll serve --config _config.yml $1
